aee030222ff1b1320735de82dc528843fd57f6ef
Lixar <fboucher@outlook.com>
2017-11-07 17:54:19 -0500

    Add project files.

---
+++ AzureBootcampAspNetCoreSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureBootcampAspNetCoreSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
